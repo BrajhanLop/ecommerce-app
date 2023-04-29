df564a54c9df371ab148b014c9fbd93592d85281
issblann <anaisabelpp@outlook.com>
2023-04-29 13:39:37 -0500

    Changes Navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export { Hero } from './Hero'
-export { Navbar } from './Navbar'
-export { Info } from './Info'
-export { Collections } from './Collections/Collections'
-export { CarrouselProducts } from './CarrouselProducts/CarrouselProducts'
-export { Follow} from './Follow/Follow'
-export { Footer } from './Footer/Footer'
+export { Hero } from "./Hero";
+export { Navbar } from "./Navbar/Index";
+export { Info } from "./Info";
+export { Collections } from "./Collections/Collections";
+export { CarrouselProducts } from "./CarrouselProducts/CarrouselProducts";
+export { Follow } from "./Follow/Follow";
+export { Footer } from "./Footer/Footer";
