f06ab9cdb8ef7fc521cb54b6c4fbb77236cb74f3
Brajhan Lopez <102929742+BrajhanLop@users.noreply.github.com>
2023-05-02 11:28:16 -0500

    Ft hero brajhan (#5)
    * Changes Navbar
    
    * add numeration hero
    
    * add eslint and prettier
    
    ---------
    
    Co-authored-by: issblann <anaisabelpp@outlook.com>

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -1,3 +1,29 @@
+import { FaBeer } from "react-icons/fa";
+
 export const Navbar = () => {
-  return <div>Navbarr</div>;
+  return (
+    <>
+      <header>
+        <span>DUROTAN</span>
+        <div>
+          <ul>
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>PRODUCT</li>
+            <li>BLOG</li>
+            <li>PAGES</li>
+          </ul>
+        </div>
+        <div>
+          <p>EN</p>
+          <p>FR</p>
+        </div>
+        <span>
+          <FaBeer />
+        </span>
+        <span></span>
+        <div></div>
+      </header>
+    </>
+  );
 };
--- src/router/EcommerceRouter.jsx
+++ src/router/EcommerceRouter.jsx
@@ -1,19 +1,14 @@
-
 import { Route, Routes } from 'react-router-dom'
-import {Home} from '../pages/Home'
+import { Home } from '../pages/Home'
 import { ListProducts } from '../pages/ListProducts'
-
-
 
 const EcommerceRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/products' element={<ListProducts/>} />
-
-        </Routes>
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<ListProducts />} />
+      </Routes>
     </>
   )
 }
